350e855ebf8dada56f56b4b4a769f4a3be1af0cf
filjon2001 <filip.jonasson@hotmail.com>
2023-07-19 13:38:35 +0200

    Setting up, getting started

---
+++ weather-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h1>Nu kör vi!</h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
