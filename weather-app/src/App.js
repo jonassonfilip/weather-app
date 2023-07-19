c437f5b91a6446509e1e03c4846becc715545dad
filjon2001 <filip.jonasson@hotmail.com>
2023-07-19 14:28:13 +0200

    Getting connected with api and making it a variable

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,7 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+/* import './App.css'; */
 
 function App() {
+
+/*   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=e037dda92e15438d349856b12b15468d` */
+
   return (
     <div className="App">
       <header className="App-header">
